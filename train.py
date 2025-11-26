7fca189a1a159e398ea9c1592c22d35b12ae73b4
akash-nepak <akashkumarnepak777@gmail.com>
2025-11-26 21:57:10 +0000

    changes

--- train.py
+++ train.py
@@ -22,9 +22,31 @@ from model import UNET
 
 path_to_data = "/home/akash-1/train_data/ADE20K"
 
-def train(batch_size =64,gradient_accumulation_steps =1,learning_rate= .001,num_epochs =100,image_size = 256,experiment_name = "unet_focal"):
+def train(batch_size =64,gradient_accumulation_steps =2,learning_rate= .001,num_epochs =100,image_size = 256,experiment_name = "unet_focal"):
      accelerator = Accelerator(gradient_accumulation_steps = gradient_accumulation_steps)
 
 
-     path_to_experiment = 
+     path_to_experiment = os.path.join("work_dir",experiment_name)
+
+     if not os.path.exists(path_to_experiment): #working directory folder to store checkpints
+          os.mkdir(path_to_experiment)
+    
+    micro_batchsize = batch_size // gradient_accumulation_steps
+
+    train_data = ADE20KDataset(path_to_data,train=True, image_size=image_size)
+    test_data = ADE20KDataset(path_to_data,train=False, image_size=image_size) 
+    trainloader = DataLoader(train_data,batch_size=micro_batchsize,shuffle=True,num_workers=8)
+    testloader = DataLoader(test_data,batch_size=micro_batchsize,shuffle = True,num_workers =8)
+
+
+     loss_fn = nn.
+
+     
+    
+
+
+
+
+
+
 
